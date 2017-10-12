88478f171baf97c6fb819a71d5d0ba1b595a934b
Leather Dog Muksihs <muksihs@gmail.com>
2017-10-11 22:31:36 -0400

    initial project checking after full repo reset

---
+++ src/main/java/muksihs/ipfs/photogallery/client/Gateways.java
@@ -0,0 +1,14 @@
+package muksihs.ipfs.photogallery.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface Gateways extends ClientBundle {
+
+	@Source("muksihs/ipfs/photogallery/shared/gateways.json")
+	TextResource gateways();
+	
+	@Source("muksihs/ipfs/photogallery/shared/writable-gateways.json")
+	TextResource writableGateways();
+
+}
---
+++ src/main/java/muksihs/ipfs/photogallery/client/PhotoGallery.java
@@ -0,0 +1,13 @@
+package muksihs.ipfs.photogallery.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class PhotoGallery implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
