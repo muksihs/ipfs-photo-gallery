530099f745d4e4d194087d06b3b6fc7a6a424a9d
Leather Dog Muksihs <muksihs@gmail.com>
2017-10-14 14:44:59 -0400

    initial element2 file list access

--- src/main/java/muksihs/ipfs/photogallery/ui/MainView.java
+++ src/main/java/muksihs/ipfs/photogallery/ui/MainView.java
@@ -6,67 +6,30 @@ import java.util.Iterator;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-import gwt.material.design.client.constants.InputType;
+import elemental2.dom.DomGlobal;
+import elemental2.dom.File;
+import elemental2.dom.FileList;
+import elemental2.dom.HTMLInputElement;
 import gwt.material.design.client.ui.MaterialButton;
-import gwt.material.design.client.ui.MaterialInput;
 import gwt.material.design.client.ui.MaterialRadioButton;
+import jsinterop.base.Js;
 import muksihs.ipfs.photogallery.client.PostingTemplates;
 
 public class MainView extends Composite {
 
-	private static MainViewUiBinder uiBinder = GWT.create(MainViewUiBinder.class);
-
 	interface MainViewUiBinder extends UiBinder<Widget, MainView> {
 	}
 
-	public MainView() {
-		initWidget(uiBinder.createAndBindUi(this));
-		Scheduler.get().scheduleDeferred(() -> {
-			rb1.addClickHandler((e) -> setColumns(1));
-			rb2.addClickHandler((e) -> setColumns(2));
-			rb4.addClickHandler((e) -> setColumns(4));
-			add.addClickHandler((e) -> upload());
-//			rb8.addClickHandler((e) -> setColumns(8));
-		});
-		Scheduler.get().scheduleDeferred(() -> {
-			rb4.setValue(true, true);
-			setColumns(4);
-		});
-	}
-
-	private Object upload() {
-		FileUpload fileUpload = new FileUpload();
-		fileUpload.getElement().setAttribute("multiple", "multiple");
-		RootPanel.get().add(fileUpload);
-		fileUpload.addChangeHandler((e)->{
-			GWT.log("fileupload-change: "+fileUpload.getFilename());
-			JsArray<JavaScriptObject> files = fileUpload.getElement().getPropertyJSO("files").cast();
-			if (files!=null) {
-				for (int ix=0; ix<files.length(); ix++) {
-					GWT.log(ix+": "+files.get(ix).toString());
-					GWT.log(ix+": "+files.get(ix).toSource());
-				}
-			}
-		});
-		return null;
-	}
-
-	private void setColumns(int columns) {
-		this.columns = columns;
-		Scheduler.get().scheduleDeferred(this::updatePreview);
-		GWT.log("Columns: " + columns);
-	};
+	private static MainViewUiBinder uiBinder = GWT.create(MainViewUiBinder.class);
 
 	private String[] pics = {
 			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00009.JPG",
@@ -78,6 +41,76 @@ public class MainView extends Composite {
 			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00012.JPG",
 			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00013.JPG"
 	};
+
+	@UiField
+	MaterialRadioButton rb1;
+
+	@UiField
+	MaterialRadioButton rb2;;
+
+	@UiField
+	MaterialRadioButton rb4;
+//	@UiField
+//	MaterialRadioButton rb8;
+
+	@UiField
+	FileUpload add;
+
+	@UiField
+	MaterialButton clear;
+	@UiField
+	MaterialButton edit;
+	@UiField
+	MaterialButton generate;
+
+	@UiField
+	DivElement picPreview;
+	private int columns = 1;
+	public MainView() {
+		initWidget(uiBinder.createAndBindUi(this));
+		Scheduler.get().scheduleDeferred(() -> {
+			rb4.setValue(true, true);
+			setColumns(4);
+		});
+		Scheduler.get().scheduleDeferred(()->{
+			add.getElement().setId("upload");
+			add.getElement().setAttribute("multiple", "multiple");
+			add.addChangeHandler((e)->{
+				GWT.log("file list changed");
+				HTMLInputElement x = (HTMLInputElement) DomGlobal.document.getElementById("upload");
+				if (x==null) {
+					Window.alert("Can't find input element!");
+				}
+				GWT.log("HTMLInputElement x: "+x.type);
+				FileList files = x.files;
+				GWT.log("Have "+files.length+" files to upload.");
+				for (int ix=0; ix<files.length; ix++) {
+					File f = files.getAt(ix);
+					GWT.log(f.name+" ["+f.type+"] "+f.lastModifiedDate);
+				}
+			});
+		});
+		Scheduler.get().scheduleDeferred(() -> {
+			rb1.addClickHandler((e) -> setColumns(1));
+			rb2.addClickHandler((e) -> setColumns(2));
+			rb4.addClickHandler((e) -> setColumns(4));
+		});
+	}
+	@Override
+	protected void onAttach() {
+		super.onAttach();
+	}
+
+	@Override
+	protected void onDetach() {
+		super.onDetach();
+	}
+
+	private void setColumns(int columns) {
+		this.columns = columns;
+		Scheduler.get().scheduleDeferred(this::updatePreview);
+		GWT.log("Columns: " + columns);
+	}
 
 	private void updatePreview() {
 		int perRow;
@@ -124,38 +157,4 @@ public class MainView extends Composite {
 		picPreview.setInnerHTML("<div>"+previewHtml.toString()+"</div>");
 		GWT.log(previewHtml.toString());
 	}
-
-	@UiField
-	MaterialRadioButton rb1;
-	@UiField
-	MaterialRadioButton rb2;
-	@UiField
-	MaterialRadioButton rb4;
-//	@UiField
-//	MaterialRadioButton rb8;
-
-	@UiField
-	MaterialMultiFileUpload add;
-	@UiField
-	MaterialButton clear;
-	@UiField
-	MaterialButton edit;
-	@UiField
-	MaterialButton generate;
-
-	@UiField
-	DivElement picPreview;
-
-	private int columns = 1;
-
-	@Override
-	protected void onAttach() {
-		super.onAttach();
-	}
-
-	@Override
-	protected void onDetach() {
-		super.onDetach();
-	}
-
 }
