b456194a345f7473da7d95844a3eeeab08b4c020
Leather Dog Muksihs <muksihs@gmail.com>
2017-10-15 19:36:16 -0400

    initial work to get files posted.

--- src/main/java/muksihs/ipfs/photogallery/ui/MainView.java
+++ src/main/java/muksihs/ipfs/photogallery/ui/MainView.java
@@ -1,28 +1,42 @@
 package muksihs.ipfs.photogallery.ui;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.builder.shared.FormBuilder;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+import elemental2.core.Function;
 import elemental2.dom.DomGlobal;
 import elemental2.dom.File;
 import elemental2.dom.FileList;
+import elemental2.dom.FormData;
 import elemental2.dom.HTMLInputElement;
+import elemental2.dom.ProgressEvent;
+import elemental2.dom.XMLHttpRequest;
+import elemental2.dom.XMLHttpRequest.OnloadCallbackFn;
 import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialRadioButton;
-import jsinterop.base.Js;
 import muksihs.ipfs.photogallery.client.PostingTemplates;
+import muksihs.ipfs.photogallery.shared.Ipfs;
+import muksihs.ipfs.photogallery.shared.IpfsGateway;
+import muksihs.ipfs.photogallery.shared.IpfsGatewayEntry;
 
 public class MainView extends Composite {
 
@@ -31,16 +45,7 @@ public class MainView extends Composite {
 
 	private static MainViewUiBinder uiBinder = GWT.create(MainViewUiBinder.class);
 
-	private String[] pics = {
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00009.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00010.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00012.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00013.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00009.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00010.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00012.JPG",
-			"https://ipfs.io/ipfs/QmWncdt35Jci678WHJCJiwTxgTLgLknvtVyuqsgAcaEWNa/slides/slide_DSC00013.JPG"
-	};
+	private List<String> pics = new ArrayList<>();
 
 	@UiField
 	MaterialRadioButton rb1;
@@ -75,19 +80,49 @@ public class MainView extends Composite {
 		Scheduler.get().scheduleDeferred(()->{
 			add.getElement().setId("upload");
 			add.getElement().setAttribute("multiple", "multiple");
+			add.getElement().setAttribute("accept", "image/*");
 			add.addChangeHandler((e)->{
 				GWT.log("file list changed");
 				HTMLInputElement x = (HTMLInputElement) DomGlobal.document.getElementById("upload");
 				if (x==null) {
 					Window.alert("Can't find input element!");
 				}
-				GWT.log("HTMLInputElement x: "+x.type);
 				FileList files = x.files;
 				GWT.log("Have "+files.length+" files to upload.");
 				for (int ix=0; ix<files.length; ix++) {
 					File f = files.getAt(ix);
+					FormData formData = new FormData();
+				    formData.append("file", f.slice());
+				     
+				    XMLHttpRequest xmlHttpRequest = new XMLHttpRequest();
+				    IpfsGatewayEntry writable = new IpfsGateway().getWritable();
+					String postUrl = writable.getBaseUrl().replace(":hash", Ipfs.EMPTY_DIR);
+					GWT.log("Posting URL: "+postUrl);
+//				    xmlHttpRequest.onerror=new Function(writable){
+//						@Override
+//						public Object apply(Object... var_args) {
+//							for (Object o: var_args) {
+//								if (o instanceof IpfsGateway) {
+//									((IpfsGatewayEntry)o).setAlive(false);
+//								}
+//							}
+//							return super.apply(var_args);
+//						}
+//				    };
+				    OnloadCallbackFn onload=new OnloadCallbackFn() {
+						@Override
+						public void onInvoke(ProgressEvent p0) {
+							GWT.log(writable.getBaseUrl()+", "+"event: "+p0.type);
+						}
+					};
+					xmlHttpRequest.onload=onload;
+					GWT.log("opening connection");
+					xmlHttpRequest.open("PUT", postUrl);
+					GWT.log("sending data");
+					xmlHttpRequest.send(formData);
 					GWT.log(f.name+" ["+f.type+"] "+f.lastModifiedDate);
 				}
+				
 			});
 		});
 		Scheduler.get().scheduleDeferred(() -> {
@@ -133,7 +168,7 @@ public class MainView extends Composite {
 			perRow=2;
 			template = StringUtils.substringBetween(template,"<!-- 2 -->", "<!--");
 		}
-		Iterator<String> iPics = Arrays.asList(pics).iterator();
+		Iterator<String> iPics = pics.iterator();
 		StringBuilder previewHtml = new StringBuilder();
 		String tmp = template;
 		int cell = 0;
