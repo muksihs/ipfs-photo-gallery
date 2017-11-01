9f06569a962c40a2a91f5d67ea8e99a0d886edc1
Leather Dog Muksihs <muksihs@gmail.com>
2017-11-01 12:06:02 -0400

    posting view adjustments

--- src/main/java/muksihs/ipfs/photogallery/ui/PostGallery.java
+++ src/main/java/muksihs/ipfs/photogallery/ui/PostGallery.java
@@ -1,11 +1,19 @@
 package muksihs.ipfs.photogallery.ui;
 
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor;
+import com.google.gwt.editor.client.EditorError;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.validation.client.impl.Validation;
 import com.google.web.bindery.event.shared.binder.EventBinder;
 
+import gwt.material.design.client.base.validator.Validator;
+import gwt.material.design.client.constants.TextAlign;
 import gwt.material.design.client.ui.MaterialIntegerBox;
 
 public class PostGallery extends EventBusComposite {
@@ -22,6 +30,16 @@ public class PostGallery extends EventBusComposite {
 		initWidget(uiBinder.createAndBindUi(this));
 		tipAmount.setMin("0");
 		tipAmount.setMax("100");
+		tipAmount.setAlignment(TextAlignment.RIGHT);
+		tipAmount.addBlurHandler((e)->{
+			if (tipAmount.getValue()<0) {
+				tipAmount.setValue(0);
+			}
+			if (tipAmount.getValue()>100) {
+				tipAmount.setValue(100);
+			}
+			tipAmount.setValue(tipAmount.getValue());
+		});
 	}
 
 	interface MyEventBinder extends EventBinder<PostGallery>{}
